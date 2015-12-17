2293b27aaee0ec3cd289c1e7912de5ce0942e723
Michael Wang <michael.icwang@gmail.com>
2015-12-17 12:38:37 +0800

    I want to see omniauth's response.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
     # render text: request.env['omniauth.auth'].to_yaml
+    p request.env['omniauth.auth'].to_yaml
     
     user = User.from_omniauth(request.env['omniauth.auth'])
     cookies[:user_id] = user.id
