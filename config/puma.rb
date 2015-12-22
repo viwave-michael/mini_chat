183fed96624b4c833d626bf7fb09a4ac803e6617
Michael Wang <michael.icwang@gmail.com>
2015-12-22 11:33:27 +0800

    Fix puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ threads Integer(ENV['MIN_THREADS'] || 1), Integer(ENV['MAX_THREADS'] || 5)
 
 preload_app!
 
-rakeup DefaultRackup
+rackup DefaultRackup
 port   ENV['PORT']          || 3000
 environment ENV['RACK_ENV'] || 'development'
 
