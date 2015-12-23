4eb9266ad6512c53f583622f0b1eecd0bbc2f9a3
Michael Wang <michael.icwang@gmail.com>
2015-12-23 11:09:03 +0800

    Get https url of user avatar.
    Heroku requires all remote resources are from https.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-    scope: 'public_profile', display: 'page', image_size: 'square'
+    scope: 'public_profile', display: 'page', image_size: 'square',
+    secure_image_url: true
 
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], image_size: 'normal'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], image_size: 'normal',
+    secure_image_url: true
 end
